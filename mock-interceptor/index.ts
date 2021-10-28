5e00326f36b4413b9d9579dbddf0cccad675a686
nunzio.zappulla <nunzio.zappulla@docebo.com>
2021-10-28 09:43:25 +0200

    interceptor 2,5/3

--- mock-interceptor/index.ts
+++ mock-interceptor/index.ts
@@ -1,11 +1,11 @@
-
-import { MockInterceptorModule, MOCKS_GROUPS } from './mock-interceptor.module';
+import { MockInterceptorModule } from './mock-interceptor.module';
 import { ADVANCEDGUSIMPORTMOCK } from './mocks/advanced-gus-import.mock';
 import { MOCKFEEDBACK } from './mocks/feedback-prodotto.mock';
 import { QUESTIONARIPRODOTTOMOCK } from './mocks/questionari-prodotto.mock';
 
 export * from './mock-interceptor.module';
 
+// this file must be replaced with index.prod.ts during production build
 export const HttpMockModule = [
   MockInterceptorModule.forRoot([ADVANCEDGUSIMPORTMOCK, MOCKFEEDBACK, QUESTIONARIPRODOTTOMOCK])
 ];
