60b3eb655adb425a000849041514f6ef11f098d4
nunzio.zappulla <nunzio.zappulla@docebo.com>
2021-11-02 13:59:28 +0100

    interceptor 2,5/3

--- mock-interceptor/widget/mock-widget.component.ts
+++ mock-interceptor/widget/mock-widget.component.ts
@@ -3,13 +3,14 @@ import {
   ChangeDetectionStrategy,
   Component,
   ComponentRef,
-  Inject,
+  Inject, NgZone,
   OnInit,
   ViewRef
 } from '@angular/core';
 import { MockService } from '../mock.service';
 import { MOCKS_GROUPS } from '../mock-interceptor.module';
 import { MappedMock, RequestMethodType, SomeKeyOfType } from '../utils/models';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'mock-widget',
@@ -17,20 +18,21 @@ import { MappedMock, RequestMethodType, SomeKeyOfType } from '../utils/models';
   styleUrls: ['./mock-widget.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MockWidgetComponent implements OnInit {
+export class MockWidgetComponent {
   activeAccordionIndex = 0;
   isOpen = false;
 
   constructor(public mockWidgetService: MockService,
               private appRef: ApplicationRef,
+              private zone: NgZone,
               @Inject(MOCKS_GROUPS) public mockGroups: SomeKeyOfType<RequestMethodType, MappedMock>[]) {
-    setTimeout( () => {
+
+    // attach view when zone tasks are empty
+    zone.onStable.pipe(take(1)).subscribe(_ => {
       this.appRef.attachView(this.mockWidgetService.componentRef.hostView);
-    })
+    });
   }
 
-  ngOnInit(): void {
-  }
 
   extractEnable(source: any, prop1: string, prop2: string, prop3: string): boolean {
     return source?.[prop1]?.[prop2]?.[prop3]
